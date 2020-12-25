527f46ccc7b285a5739765f07b198c8406b4c58f
Cüneyt <kayacuneyd@gmail.com>
2020-12-25 17:07:19 +0100

    first commit

---
+++ frontity.settings.js
@@ -0,0 +1,65 @@
+const settings = {
+  "name": "my-frontity-project",
+  "state": {
+    "frontity": {
+      "url": "http://front.trendpatik.com",
+      "title": "Kaya's World of Programmingtrue",
+      "description": false
+    }
+  },
+  "packages": [
+    {
+      "name": "@frontity/mars-theme",
+      "state": {
+        "theme": {
+          "menu": [
+            [
+              "Home",
+              "/"
+            ],
+            [
+              "Anasayfa",
+              "/home/"
+            ],
+            [
+              "About Me",
+              "/about-me/"
+            ],
+            [
+              "Programming Basics",
+              "/category/programming/"
+            ],
+            [
+              "Some CSS",
+              "/category/css/"
+            ],
+            [
+              "WordPress",
+              "/category/wordpress/"
+            ],
+            [
+              "Contact",
+              "/contact/"
+            ]
+          ],
+          "featured": {
+            "showOnList": true,
+            "showOnPost": true
+          }
+        }
+      }
+    },
+    {
+      "name": "@frontity/wp-source",
+      "state": {
+        "source": {
+          "url": "http://front.trendpatik.com/"
+        }
+      }
+    },
+    "@frontity/tiny-router",
+    "@frontity/html2react"
+  ]
+};
+
+export default settings;
